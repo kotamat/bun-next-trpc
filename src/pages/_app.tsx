d0ce21c343a5f301ab8f8f65cd7689abb8cc9f1c
kotamat <kota1861@gmail.com>
2022-08-14 14:28:09 +0900

    Configure _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,31 @@
+import { withTRPC } from "@trpc/next";
 import "../styles/globals.css";
+import { AppRouter } from "./api/trpc/[trpc]";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+const getBaseUrl = () => {
+  if (typeof window != "undefined") {
+    return "";
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  if (process.env.RENDER_INTERNAL_HOSTNAME) {
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+};
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    return {
+      url: `${getBaseUrl()}/api/trpc`,
+    };
+  },
+  ssr: true,
+})(MyApp);
